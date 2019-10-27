388ef34af636cac869df36ec0058d607e1c33218
Craig <craig.slingerland@gmail.com>
2019-10-26 22:20:45 -0400

    added output to file

--- base_text_config_parser.py
+++ base_text_config_parser.py
@@ -9,18 +9,22 @@ out_data = []
 
 if delimiter_char:
     data = open(input_file, 'r').read().split(delimiter_char)
+    out = open(output_file, 'a')
     for section in data:
         # breaks sections into lines to output somewhere
         for line in section.split('\n'):
             # look for whatever you want to output
             if 'no' in line:
                 out_data.append(line)
+                out.write(line + '\n')
                 print(out_data)
 else:
     data = open(input_file, 'r').readlines()
     # goes through each line to output somewhere
+    out = open(output_file, 'a')
     for line in data:
         # look for whatever you want to output
         if 'no' in line:
             out_data.append(line.strip('\n'))
+            out.write(line)
             print(out_data)
